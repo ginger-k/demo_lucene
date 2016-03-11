7137a545267dff26032f29aada75f3484bd24ed2
ginger-k <ywkxwx@163.com>
2016-03-11 10:18:55 +0800

    change class, add sql

---
+++ lib/lucene.sql
@@ -0,0 +1,58 @@
+/*
+SQLyog Ultimate v11.27 (32 bit)
+MySQL - 5.6.29 : Database - lucene
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`lucene` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `lucene`;
+
+/*Table structure for table `t_item` */
+
+DROP TABLE IF EXISTS `t_item`;
+
+CREATE TABLE `t_item` (
+  `id` varchar(32) NOT NULL COMMENT '主见id',
+  `name` varchar(64) NOT NULL COMMENT '术语名称',
+  `description` varchar(1024) NOT NULL COMMENT '术语解释',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `t_procedure` */
+
+DROP TABLE IF EXISTS `t_procedure`;
+
+CREATE TABLE `t_procedure` (
+  `id` varchar(32) NOT NULL COMMENT '主键id',
+  `name` varchar(128) NOT NULL COMMENT '名称',
+  `content` varchar(1024) NOT NULL COMMENT '内容',
+  `path` varchar(512) NOT NULL COMMENT '路径',
+  `createTime` datetime NOT NULL COMMENT '创建时间',
+  `updateTime` datetime NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `t_procedure_item` */
+
+DROP TABLE IF EXISTS `t_procedure_item`;
+
+CREATE TABLE `t_procedure_item` (
+  `id` varchar(32) NOT NULL COMMENT '主键id',
+  `procedure_id` varchar(32) NOT NULL COMMENT '程序id',
+  `item_id` varchar(32) NOT NULL COMMENT '术语id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
--- src/main/java/com/ceair/lucene/service/ProcedureService.java
+++ src/main/java/com/ceair/lucene/service/ProcedureService.java
@@ -1,32 +1,10 @@
 package com.ceair.lucene.service;
 
-import java.io.File;
-import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.StringField;
-import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.index.Term;
-import org.apache.lucene.index.IndexWriterConfig.OpenMode;
-import org.apache.lucene.queryparser.classic.ParseException;
-import org.apache.lucene.search.BooleanQuery;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.TermQuery;
-import org.apache.lucene.search.TopDocs;
-import org.apache.lucene.search.BooleanClause.Occur;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.util.Version;
-import org.wltea.analyzer.lucene.IKAnalyzer;
 
 import com.ceair.lucene.pojo.Item;
 import com.ceair.lucene.pojo.Procedure;
@@ -37,35 +15,32 @@ public class ProcedureService {
 	
 	/* 保存程序
 	 * 1.如果已经存在，就不添加
-	 * 2.程序保存后，将程序索引
-	 * 3.迭代术语库，结合程序ID，生成该程序的术语库
+	 * 2.程序保存
+	 * 3.迭代术语库，结合程序内容，生成该程序的术语库
 	 * 4.往t_procedure_item表中建立手册和术语的关系
 	 */
-	public List<Item> addProcedure(Procedure procedure) throws SQLException, IOException, ParseException {
+	public List<Item> addProcedure(Procedure procedure) throws SQLException {
 		if (procedure  == null || procedure.getName() == null 
 				|| procedure.getPath() == null || procedure.getClass() == null) {
 			throw new IllegalArgumentException();
 		}
 		
 		//1.如果已经存在，就不添加
-		Procedure oldProcedure = this.getItemByName(procedure.getName());
+		Procedure oldProcedure = this.getProcedureByName(procedure.getName());
 		if (oldProcedure != null) {
 			throw new IllegalArgumentException();
 		}
-		String sql = "INSERT INTO t_procedure (id,name,path) VALUES(?,?,?)";
+		String sql = "INSERT INTO t_procedure (id,name,content,path,createTime,updateTime) VALUES(?,?,?,?,?,?)";
 		if (StringUtils.isBlank(procedure.getId())) {
 			procedure.setId(IdUtil.generatorId());
 		}
-		long start0 = System.currentTimeMillis();
-		JdbcUtil.update(sql, procedure.getId(), procedure.getName(), procedure.getPath());
-		long end0 = System.currentTimeMillis();
-		System.out.println("插入数据到程序表花费：" + (end0 - start0) + "ms");
 		
-		//2.程序保存后，将程序索引
+		//2.程序保存
 		long start1 = System.currentTimeMillis();
-		this.createIndex(procedure);
+		JdbcUtil.update(sql, procedure.getId(), procedure.getName(), procedure.getContent(), 
+				procedure.getPath(), procedure.getCreateTime(), procedure.getUpdateTime());
 		long end1 = System.currentTimeMillis();
-		System.out.println("建立索引花费：" + (end1 - start1) + "ms");
+		System.out.println("插入数据到程序表花费：" + (end1 - start1) + "ms");
 		
 		//3.迭代术语库，结合程序ID，生成该程序的术语列表
 		long start2 = System.currentTimeMillis();
@@ -77,16 +52,17 @@ public class ProcedureService {
 		if (items != null && items.size() > 0) {
 			long start3 = System.currentTimeMillis();
 			for (Item item : items) {
-				boolean has = this.indexHasItem(item.getName(), procedure.getId());
-				if (has) {
+				String content = procedure.getContent();
+				String name = item.getName();
+				if (content.contains(name)) {
 					list.add(item);
 				}
 			}
 			long end3 = System.currentTimeMillis();
-			System.out.println("检索索引花费：" + (end3 - start3) + "ms");
+			System.out.println("内容字符串检索索引花费：" + (end3 - start3) + "ms");
 		}
 		
-		//4.往t_procedure_item表中建立手册和术语的关系
+		//4.往 t_procedure_item表中建立手册和术语的关系
 		long start4 = System.currentTimeMillis();
 		this.addProcedureItem(procedure, list);
 		long end4 = System.currentTimeMillis();
@@ -94,7 +70,7 @@ public class ProcedureService {
 		return list;
 	}
 	
-	public Procedure getItemByName(String name) throws SQLException {
+	public Procedure getProcedureByName(String name) throws SQLException {
 		long start = System.currentTimeMillis();
 		String sql = "SELECT id,name,path FROM t_procedure WHERE name = ?";
 		Procedure pojo = JdbcUtil.getPojo(sql, Procedure.class, name);
@@ -103,37 +79,6 @@ public class ProcedureService {
 		return pojo;
 	}
 	
-	private void createIndex(Procedure procedure) throws IOException {
-		Document document = new Document();// 商品数据
-        document.add(new StringField("id", procedure.getId(), Field.Store.YES));
-        document.add(new StringField("name", procedure.getName(), Field.Store.YES));
-        document.add(new TextField("content", procedure.getContent(), Field.Store.YES));
-        document.add(new StringField("path", procedure.getPath(), Field.Store.YES));
-        Directory directory = FSDirectory.open(new File("index"));
-        Analyzer analyzer = new IKAnalyzer();
-        IndexWriterConfig indexWriterConfig = new IndexWriterConfig(Version.LUCENE_4_10_2, analyzer);
-        indexWriterConfig.setOpenMode(OpenMode.CREATE_OR_APPEND);
-        IndexWriter indexWriter = new IndexWriter(directory, indexWriterConfig);
-        indexWriter.addDocument(document);
-        indexWriter.close();
-	}
-	
-	private boolean indexHasItem(String item, String procedureId) throws IOException {
-		Directory directory = FSDirectory.open(new File("index"));
-        IndexSearcher indexSearcher = new IndexSearcher(DirectoryReader.open(directory));
-        BooleanQuery query = new BooleanQuery();
-        TermQuery query1 = new TermQuery(new Term("id", procedureId));
-        TermQuery query2 = new TermQuery(new Term("content", item));
-        query.add(query1, Occur.MUST);
-        query.add(query2, Occur.MUST);
-        TopDocs topDocs = indexSearcher.search(query, 1);
-        int hit = topDocs.totalHits;
-        if (hit == 1) {
-        	return true;
-        } else {
-        	return false;
-        }
-	}
 	
 	private void addProcedureItem(Procedure procedure, List<Item> items) throws SQLException {
 		String sql = "insert into t_procedure_item (id, procedure_id, item_id) values(? ,? ,?)";
